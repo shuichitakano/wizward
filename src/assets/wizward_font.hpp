ac867d0c751b18f9260af764eb5f8e295025512b
Shuichi Takano <shuichi.takano@gmail.com>
2026-07-18 09:29:09 -0700

    Wizward用ビットマップフォントを追加

---
+++ src/assets/wizward_font.hpp
@@ -0,0 +1,12 @@
+// このファイルはfont_converter.pyにより生成されました。編集しないでください。
+#pragma once
+
+#include "pixel_twins/font.hpp"
+
+namespace wizward {
+namespace assets {
+
+extern const pixel_twins::BitmapFont kWizwardFont;
+
+} // namespace assets
+} // namespace wizward
